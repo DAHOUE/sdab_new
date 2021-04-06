f8da0cf40af679b074a11bd2ecd0d620f28e772d
Isaac <isaacdahoue1@gmail.com>
2021-04-06 08:59:50 +0100

    second commit

--- lib/Screen/Welcome/Component/body.dart
+++ lib/Screen/Welcome/Component/body.dart
@@ -7,13 +7,83 @@ import 'package:sdab_new/Screen/Login/login_screen.dart';
 import 'package:sdab_new/Screen/Welcome/Component/background.dart';
 import 'package:sdab_new/constants.dart';
 
-class Body extends StatelessWidget {
+
+
+class Body extends StatefulWidget {
+  @override
+  _BodyStateState createState() => _BodyStateState();
+}
+
+class _BodyStateState extends State<Body> {
+
+  bool loginClicked = false;
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Background(
-      child1: Column(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+
+        children: [
+          Container(
+            padding: EdgeInsets.all(40.0),
+            width: size.width,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Image.asset("assets/IMG-20210325-WA0017.jpg", height: size.width * 0.50,),
+                TextWithStyle(text: loginClicked? "Connectez-vous sur votre compte" : "Créer un compte                    ",
+                    fontWeight: FontWeight.bold,
+                    fontSize: size.height * 0.05),
+                Divider(thickness: 5,
+                  height: 30,
+                  color: color_black,
+                  endIndent: size.width * 0.80,),
+                TextWithStyle(text: "Pour accéder aux fonctionnalités importantes de l'application",
+                  fontSize: size.height * 0.03,
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: size.width * 0.15),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                TextButton(onPressed: (){
+                  setState(() {
+                    loginClicked = false;
+                  });
+                }, child: TextWithStyle(text: "Inscription",)),
+                SizedBox(width: 75.0,),
+                TextButton(onPressed: (){
+                  setState(() {
+                    loginClicked = true;
+                  });
+                }, child: TextWithStyle(text: "Connexion",))
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: color_green,
+              width: size.width,
+              height: size.height / 3,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  RoundedTextButton(text: loginClicked ? "Connectez-vous ici" : "Inscrevez-vous ici",)
+                ],
+              ),
+            ),
+          )
+        ],
+
+      ),
+
+      /*child1: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
           Image.asset("assets/IMG-20210325-WA0017.jpg", height: size.width * 0.50,),
@@ -37,7 +107,7 @@ class Body extends StatelessWidget {
             RoundedTextButton(text: "Connectez-vous ici",)
           ],
         ),
-      ),
+      ),*/
     );
   }
 }
